b36030a7ced5e77fc92b4a992eb14d4a6c8ca321
adamzw33 <azahwee73@hotmail.com>
2025-11-25 11:30:11 -0700

    client template update

---
+++ Client.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+import os
+
+
+def client():
+    serverName = 'localhost'
+    serverPort = 13000
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as err:
+        print(f"Socket creation error: {e}")
+        sys.exit(1)
